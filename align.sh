99c23aa40148d89da38764c15c90ae63be363d57
webees <webees@qq.com>
2019-12-22 15:02:27 +0800

    # v0.0.1

--- align.sh
+++ align.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 source f.sh
+# v0.0.1
+# ./align.sh tmp/boot.bin
 
-# ./align.sh tmp/boot.bin dist/_boot.bin 32768
+readonly OUT_FILE_MARK=$2
+readonly OUT_FILE_PATH="dist"
+readonly OUT_FILE_NAME="_boot.bin"
 
-main() {
-  rm -f $2
-  cp -f -p $1 $2
-  readonly total_size=$3
-  readonly file_size=$(file_len $2)
-  readonly fill_size=$[total_size - $file_size]
-  readonly zero_num=$[fill_size * 2]
-  readonly zero_str=$(to_hex 0 $zero_num)
-  echo $zero_str | xxd -r -ps >> $2
-  file_info $1
-  file_info $2
-}
+readonly SRC_File=$1
+readonly OUT_FILE="$OUT_FILE_PATH/$OUT_FILE_NAME"
 
-main "$@"
+readonly SRC_File_SIZE=$(file_len $SRC_File)
+readonly OUT_FILE_SIZE=32768
+
+readonly fill_size=$[OUT_FILE_SIZE - $SRC_File_SIZE]
+readonly zero_num=$[fill_size * 2]
+readonly zero_str=$(to_hex 0 $zero_num)
+
+cp -f -p $SRC_File $OUT_FILE && $(echo $zero_str | xxd -r -ps >> $OUT_FILE)
+
+file_info $SRC_File
+file_info $OUT_FILE
--- boot.sh
+++ boot.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 source f.sh
-
+# v0.0.1
 # ./boot.sh tmp/boot.dat v1.0.0
 
+readonly SRC_File=$1
 readonly OUT_FILE_MARK=$2
 readonly OUT_FILE_NAME="bl_data.h"
 readonly OUT_FILE_PATH="dist"
 readonly OUT_FILE="$OUT_FILE_PATH/$OUT_FILE_NAME"
 
-main() {
-  rm -f $OUT_FILE
-  file_info $1
-  readonly bl_data=$(xxd -ps -c 1 $1 | awk '{printf"0x%s,",$0}')
-  readonly bl_sha256d=`sha256d $1`
-  readonly bl_hash=$(echo -n $bl_sha256d | xxd -r -ps | xxd -ps -c 1 | awk '{printf"0x%s,",$0}')
-  echo "
+readonly bl_data=$(xxd -ps -c 1 $SRC_File | awk '{printf"0x%s,",$0}')
+readonly bl_hash=$(echo -n $(sha256d $SRC_File) | xxd -r -ps | xxd -ps -c 1 | awk '{printf"0x%s,",$0}')
+
+cat << EOF > $OUT_FILE
 // $OUT_FILE_MARK
 static const uint8_t bl_data[32768] = {$bl_data};
 static const uint8_t bl_hash[32] = {$bl_hash};
-" > $OUT_FILE
-  file_info $OUT_FILE
-}
+EOF
 
-main "$@"
+file_info $SRC_File
+file_info $OUT_FILE
